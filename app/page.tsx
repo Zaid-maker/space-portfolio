d022b87d5ab75a5059d26514de0fbe787bc36205
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-06 18:39:43 +0000

    update the hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "@/components/main/Hero";
+
 export default function Home() {
   return (
     <main className="w-full h-full">
-      <div className="flex flex-col gap-20">Space Portfolio</div>
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
     </main>
   );
 }
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+    </div>
+  );
 };
 
 export default Hero;
