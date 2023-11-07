c85a65cfbe611f36c26694241a848ec68df1660c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-07 14:53:15 +0000

    add a SparklesIcon

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -5,6 +5,7 @@ import {
   slideInFromRight,
   slideInFromTop,
 } from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -20,7 +21,7 @@ const HeroContent = () => {
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          {/*<SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />*/}
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
             Fullstack Developer Portfolio
           </h1>
