c8abdee8a9b7018305bce16043261635965d4f4a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-07 14:49:31 +0000

    add animation on top

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -12,7 +16,11 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          {/*<SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />*/}
           <h1 className="Welcome-text text-[13px]">
             Fullstack Developer Portfolio
           </h1>
