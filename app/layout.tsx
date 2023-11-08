fca3557e23a1955934e9c7dfc5811b5ba4b0ff75
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-08 14:36:19 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas />
         {children}
       </body>
     </html>
