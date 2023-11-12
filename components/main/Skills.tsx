eaf0bf5396c276f5582ed6ed94b04c746796940c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-12 21:00:56 +0000

    update

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,7 +1,20 @@
+import { Skill_data } from "@/constants";
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section
+      id="skills"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+      style={{ transform: "scale(0.9" }}
+    >
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Skill_data.map((image, index) => (
+          <div key={index}>test</div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
