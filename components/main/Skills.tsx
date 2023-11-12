aafe7225300a344948e5cffc9afeed1a681c6251
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-12 21:13:10 +0000

    create SkillText component

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/constants";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
+import SkillsText from "../sub/SkillsText";
 
 const Skills = () => {
   return (
@@ -15,6 +16,8 @@ const Skills = () => {
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9" }}
     >
+      <SkillsText />
+
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Skill_data.map((image, index) => (
           <SkillDataProvider
---
+++ components/sub/SkillsText.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const SkillsText = () => {
+  return <div>SkillsText</div>;
+};
+
+export default SkillsText;
