41d325ca0972e88a0a8cdd41b152edaa3f009743
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-12 21:16:06 +0000

    update

--- components/sub/SkillsText.tsx
+++ components/sub/SkillsText.tsx
@@ -1,9 +1,24 @@
 "use client";
 
 import React from "react";
+import { motion } from "framer-motion";
+import { slideInFromTop } from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
 
 const SkillsText = () => {
-  return <div>SkillsText</div>;
+  return (
+    <div className="w-full h-auto flex flex-col items-center justify-center">
+      <motion.div
+        variants={slideInFromTop}
+        className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+      >
+        <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+        <h1 className="Welcome-text text-[13px]">
+          Think better with Next js 13
+        </h1>
+      </motion.div>
+    </div>
+  );
 };
 
 export default SkillsText;
